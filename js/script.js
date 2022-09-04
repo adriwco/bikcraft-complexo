4998a44b4a1475b863b537568d92888e72ba5c5e
adriwco <adriwco@gmail.com>
2022-09-04 16:00:22 -0300

    galeria-img efeito troca ordem

--- js/script.js
+++ js/script.js
@@ -23,13 +23,36 @@ parametros.forEach(ativarProduto);
 const perguntas = document.querySelectorAll('.perguntas button');
 function ativarPergunta(event){
   const pergunta = event.currentTarget; //pergando o elemento
-  const controls = pergunta.getAttribute('aria-controls');
-  const resposta = document.getElementById(controls);
+  const controls = pergunta.getAttribute('aria-controls'); // aria-controls="..."
+  const resposta = document.getElementById(controls); // controls -> id resposta
   resposta.classList.toggle("ativa");
-  const ativa = resposta.classList.contains("ativa");
-  pergunta.setAttribute("aria-expanded", ativa);
+  const ativa = resposta.classList.contains("ativa"); // retorno booleano -> existencia!
+  pergunta.setAttribute("aria-expanded", ativa); // aria-expanded="...", indicar se estar expandido(true) ou comprimido(false)...
 }
 function eventosPerguntas(pergunta){
   pergunta.addEventListener('click', ativarPergunta);
 }
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de bicicleta
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function trocarImagem(event){
+  const img = event.currentTarget;
+  const media = window.matchMedia('(min-width: 1000px)').matches;
+  if(media){
+    galeriaContainer.prepend(img); // remove o elemento e cola no primeiro lugar
+  }
+}
+
+function eventosGaleria(img){
+  img.addEventListener('click', trocarImagem)
+}
+
+galeria.forEach(eventosGaleria);
+
+// galeria.forEach(() => {
+// arrow funciton or "funçao flecha"  
+//  =>  : Isto é uma função lambda, ou como costuma ser chamada, função flecha. É uma função anônima com uma sintaxe mais simples. disponível desde EcmaScript 6. 
+// });
